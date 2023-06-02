50cb1dd046ece57c3840938d5d2328441e23656b
Santosh Raju <sraju@newrelic.com>
2023-06-02 16:21:02 -0600

    Updates to AdError

--- nerdlets/shared/config/constants.js
+++ nerdlets/shared/config/constants.js
@@ -1,3 +1,3 @@
 export const USER_IDENTIFIER = 'userId'
-export const FIND_USER_ATTRIBUTE = ['userId'] // ['tvAccount', 'aisuid']
+export const FIND_USER_ATTRIBUTE = ['userId', 'userAisuid']
 export const VIDEO_EVENTS = ' PageAction, MobileVideo, RokuVideo, BrowserVideo '
